dad63c193b588f11ecff769cd167ae537da30c22
David Hardenbrook <dhardenb@Davids-iMac.local>
2011-11-13 17:44:35 -0600

    Changed the scope to be round instead of square and to take up the whole viewing area.

--- scripts/gui.js
+++ scripts/gui.js
@@ -3,18 +3,15 @@ function renderScope(parentNode)
   // Group
   var ScopeGroupElement = document.createElementNS(svgNS,"g");
   x = componentOffset;
-  y = AvailablePixels * .1 + componentOffset;
+  y = componentOffset;
   ScopeGroupElement.setAttribute('transform', 'translate(' + x + ',' + y +')');
   parentNode.appendChild(ScopeGroupElement);
   
     // Background
-    var ScopeElement = document.createElementNS(svgNS,"rect");
-    ScopeElement.setAttributeNS(null, "x", 0);	
-    ScopeElement.setAttributeNS(null, "y", 0);		
-    ScopeElement.setAttributeNS(null, "rx", roundingConstant);
-    ScopeElement.setAttributeNS(null, "ry", roundingConstant);
-    ScopeElement.setAttributeNS(null, "height", AvailablePixels * .5 - componentOffset);
-    ScopeElement.setAttributeNS(null, "width", AvailablePixels - componentOffset * 2);
+    var ScopeElement = document.createElementNS(svgNS,"circle");
+    ScopeElement.setAttributeNS(null, "cx", (AvailablePixels - componentOffset * 2) / 2);	
+    ScopeElement.setAttributeNS(null, "cy", (AvailablePixels - componentOffset * 2) / 2);		
+    ScopeElement.setAttributeNS(null, "r", (AvailablePixels - componentOffset * 2) / 2);
     ScopeElement.setAttributeNS(null, "stroke", "gray");
     ScopeElement.setAttributeNS(null, "stroke-width", "2px");
     ScopeElement.setAttributeNS(null, "fill", "dimgray");
@@ -26,25 +23,22 @@ function renderScope(parentNode)
     Map_Viewport.setAttributeNS(null, "y", componentOffset);		
     Map_Viewport.setAttributeNS(null, "rx", roundingConstant);
     Map_Viewport.setAttributeNS(null, "ry", roundingConstant);
-    Map_Viewport.setAttributeNS(null, "height", AvailablePixels * .5 - componentOffset * 3);
+    Map_Viewport.setAttributeNS(null, "height", AvailablePixels - componentOffset * 4);
     Map_Viewport.setAttributeNS(null, "width", AvailablePixels - componentOffset * 4);
     ScopeGroupElement.appendChild(Map_Viewport);
   
       // scopeScreen
-      var scopeScreen = document.createElementNS(svgNS,"rect");
-      scopeScreen.setAttributeNS(null, "x", 0);	
-      scopeScreen.setAttributeNS(null, "y", 0);		
-      scopeScreen.setAttributeNS(null, "rx", roundingConstant);
-      scopeScreen.setAttributeNS(null, "ry", roundingConstant);
-      scopeScreen.setAttributeNS(null, "height", AvailablePixels * .5 - componentOffset * 3);
-      scopeScreen.setAttributeNS(null, "width", AvailablePixels - componentOffset * 4);
+      var scopeScreen = document.createElementNS(svgNS,"circle");
+      scopeScreen.setAttributeNS(null, "cx", (AvailablePixels - componentOffset * 4) / 2);	
+      scopeScreen.setAttributeNS(null, "cy", (AvailablePixels - componentOffset * 4) / 2);		
+      scopeScreen.setAttributeNS(null, "r", (AvailablePixels - componentOffset * 4) / 2);
       scopeScreen.setAttributeNS(null, "fill", "black");
       Map_Viewport.appendChild(scopeScreen);
       
       // Map
       var centerMap = document.createElementNS(svgNS,"g");
       x = ((AvailablePixels - componentOffset) * 0.5);
-      y = ((AvailablePixels - componentOffset) * 0.25);
+      y = ((AvailablePixels - componentOffset) * 0.5);
       centerMap.setAttribute('transform', 'translate('+ x +','+ y +') scale(' + CurrentScale + ')');
       Map_Viewport.appendChild(centerMap);
 
