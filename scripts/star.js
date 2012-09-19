e0eacebf9baf3378d75eadf786d0d9e135183a63
David Hardenbrook <dhardenb@Davids-iMac-2.local>
2012-09-19 00:03:43 -0500

    Switch Start object back from Backbone to a classic "class"

--- scripts/star.js
+++ scripts/star.js
@@ -1,69 +1,21 @@
-var StarModel = Backbone.Model.extend(
+// star.js
+
+function Star(star)
 {
-	defaults:
-	{
-		xLocation: 0,
-		yLocation: 0,
-		radias: 1,
-		fill: "white",
-		alpha: 1.0,
-		brightning: 0,
-		twinkleRate: 0.05
-	},
-					
-	twinkle: function()
-	{
-		if (this.get("brightning") == 0)
-		{
-			if (this.get("alpha") < 1.0)
-			{
-				this.set("alpha",  this.get("alpha") + this.get("twinkleRate"));
-			}
-			else 
-			{
-				this.set("alpha", 0.99);
-				this.set("brightning", 1);
-			}
-		}
-		
-		else // (this.brightning == 1)
-		{
-			if (this.get("alpha") > 0.5)
-			{
-				this.set("alpha", this.get("alpha") - this.get("twinkleRate"));
-			}
-			else 
-			{
-				this.set("alpha", 0.55);
-				this.set("brightning", 0);
-			}
-		}	
-	}
-});
+	this.xLocation = star.xLocation;
+	this.yLocation = star.yLocation;
+	this.alpha = Math.random();
 	
-var StarView = Backbone.View.extend(
+	this.createView();
+}
+
+Star.prototype.createView = function()
 {
-	initialize: function()
-	{
-		this.model.bind("change:alpha", this.update, this);
-		this.render();
-	},
-		
-	render: function()
-	{
-		this.el = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-			
-		this.el.setAttributeNS(null, "cx", this.model.get("xLocation"));	
-		this.el.setAttributeNS(null, "cy", this.model.get("yLocation"));	
-		this.el.setAttributeNS(null, "r", this.model.get("radias"));	
-		this.el.setAttributeNS(null, "fill", this.model.get("fill"));
-		this.el.setAttributeNS(null, "fill-opacity", this.model.get("alpha"));
-			
-		backgroundGroup.appendChild(this.el);
-	},
-		
-	update: function()
-	{
-		this.el.setAttributeNS(null, "fill-opacity", this.model.get("alpha"));
-	}
-});
+	this.svgElement = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	this.svgElement.setAttributeNS(null, "cx", this.xLocation);	
+	this.svgElement.setAttributeNS(null, "cy", this.yLocation);	
+	this.svgElement.setAttributeNS(null, "r", "1");	
+	this.svgElement.setAttributeNS(null, "fill", "white");
+	this.svgElement.setAttributeNS(null, "fill-opacity", this.alpha);
+	backgroundGroup.appendChild(this.svgElement);
+}
