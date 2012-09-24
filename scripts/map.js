da6e3094dad18f6ae31f17375b68a6f6a5d144f3
David Hardenbrook <dhardenb@Davids-iMac-2.local>
2012-09-23 23:40:36 -0500

    Added the scope mask

--- scripts/map.js
+++ scripts/map.js
@@ -16,22 +16,6 @@ Map.prototype.create = function()
 	backgroundGroup.setAttribute('id', 'backgroundGroup');
 	background.appendChild(backgroundGroup);
   
-	scopeGroup = document.createElementNS("http://www.w3.org/2000/svg","g");
-	scopeGroup.setAttribute('id', 'scopeGroup');
-	scopeGroup.setAttribute('transform', 'translate('+availableWidth / 2+','+availableHeight / 2+')');
-	background.appendChild(scopeGroup);
-  
-	scope = document.createElementNS("http://www.w3.org/2000/svg","circle");
-	scope.setAttributeNS(null, "cx", 0);	
-	scope.setAttributeNS(null, "cy", 0);		
-	scope.setAttributeNS(null, "r", ((availablePixels - 22) / 2));
-	scope.setAttributeNS(null, "stroke", "gray");
-	scope.setAttributeNS(null, "stroke-width", "2px");
-	scope.setAttributeNS(null, "stroke-opacity", 0.5);
-	scope.setAttributeNS(null, "fill", "black");
-	scope.setAttributeNS(null, "fill-opacity", 0.0);
-	scopeGroup.appendChild(scope);
-  
 	portGroup = document.createElementNS("http://www.w3.org/2000/svg","g");
 	portGroup.setAttribute('id', 'portGroup');
 	portGroup.setAttribute('transform', 'translate('+availableWidth / 2+','+availableHeight / 2+') scale(' + currentScale + ')');
@@ -48,6 +32,30 @@ Map.prototype.create = function()
 	mapGroup = document.createElementNS("http://www.w3.org/2000/svg","g");
 	mapGroup.setAttribute('id', 'mapGroup');
 	translateGroup.appendChild(mapGroup);
+	
+	scopeGroup = document.createElementNS("http://www.w3.org/2000/svg","g");
+	scopeGroup.setAttribute('id', 'scopeGroup');
+	scopeGroup.setAttribute('transform', 'translate('+availableWidth / 2+','+availableHeight / 2+')');
+	background.appendChild(scopeGroup);
+  
+	test = document.createElementNS("http://www.w3.org/2000/svg","path");
+    test.setAttributeNS(null, 'stroke', 'black');  
+    test.setAttributeNS(null, 'd', 'M '+availableWidth/2*-1+','+availableHeight/2*-1+' L '+availableWidth+','+availableHeight/2*-1+' L '+availableWidth+','+availableHeight+' L '+ availableWidth/2*-1 +','+availableHeight+' L '+availableWidth/2*-1+','+availableHeight/2*-1+' M 0, 0 m '+(((availablePixels - 22) / 2)*-1)+', 0 a '+((availablePixels - 22) / 2)+','+((availablePixels - 22) / 2)+' 0 1,0 '+(availablePixels - 22)+',0 a '+(availablePixels - 22)/2+','+(availablePixels - 22)/2+' 0 1,0 '+(availablePixels - 22)*-1+',0');
+    test.setAttributeNS(null, 'stroke-linejoin', 'round');
+    test.setAttributeNS(null, 'stroke-width', 2 / currentScale);
+    test.setAttributeNS(null, 'fill', 'black');
+    scopeGroup.appendChild(test);
+    
+    scope = document.createElementNS("http://www.w3.org/2000/svg","circle");
+	scope.setAttributeNS(null, "cx", 0);	
+	scope.setAttributeNS(null, "cy", 0);		
+	scope.setAttributeNS(null, "r", ((availablePixels - 22) / 2));
+	scope.setAttributeNS(null, "stroke", "gray");
+	scope.setAttributeNS(null, "stroke-width", "2px");
+	scope.setAttributeNS(null, "stroke-opacity", 0.5);
+	scope.setAttributeNS(null, "fill", "black");
+	scope.setAttributeNS(null, "fill-opacity", 0.0);
+	scopeGroup.appendChild(scope);
 }
 
 Map.prototype.createStars = function()
