400b76e28fb293e04f722499de2562ece16ef1cc
David Hardenbrook <dhardenb@Davids-iMac-2.local>
2012-09-24 00:01:47 -0500

    Added additional grid lines to scope.

--- scripts/map.js
+++ scripts/map.js
@@ -38,24 +38,42 @@ Map.prototype.create = function()
 	scopeGroup.setAttribute('transform', 'translate('+availableWidth / 2+','+availableHeight / 2+')');
 	background.appendChild(scopeGroup);
   
-	test = document.createElementNS("http://www.w3.org/2000/svg","path");
-    test.setAttributeNS(null, 'stroke', 'black');  
-    test.setAttributeNS(null, 'd', 'M '+availableWidth/2*-1+','+availableHeight/2*-1+' L '+availableWidth+','+availableHeight/2*-1+' L '+availableWidth+','+availableHeight+' L '+ availableWidth/2*-1 +','+availableHeight+' L '+availableWidth/2*-1+','+availableHeight/2*-1+' M 0, 0 m '+(((availablePixels - 22) / 2)*-1)+', 0 a '+((availablePixels - 22) / 2)+','+((availablePixels - 22) / 2)+' 0 1,0 '+(availablePixels - 22)+',0 a '+(availablePixels - 22)/2+','+(availablePixels - 22)/2+' 0 1,0 '+(availablePixels - 22)*-1+',0');
-    test.setAttributeNS(null, 'stroke-linejoin', 'round');
-    test.setAttributeNS(null, 'stroke-width', 2 / currentScale);
-    test.setAttributeNS(null, 'fill', 'black');
-    scopeGroup.appendChild(test);
+	mask = document.createElementNS("http://www.w3.org/2000/svg","path");
+    mask.setAttributeNS(null, 'stroke', 'black');  
+    mask.setAttributeNS(null, 'd', 'M '+availableWidth/2*-1+','+availableHeight/2*-1+' L '+availableWidth+','+availableHeight/2*-1+' L '+availableWidth+','+availableHeight+' L '+ availableWidth/2*-1 +','+availableHeight+' L '+availableWidth/2*-1+','+availableHeight/2*-1+' M 0, 0 m '+(((availablePixels - 22) / 2)*-1)+', 0 a '+((availablePixels - 22) / 2)+','+((availablePixels - 22) / 2)+' 0 1,0 '+(availablePixels - 22)+',0 a '+(availablePixels - 22)/2+','+(availablePixels - 22)/2+' 0 1,0 '+(availablePixels - 22)*-1+',0');
+    mask.setAttributeNS(null, 'stroke-linejoin', 'round');
+    mask.setAttributeNS(null, 'stroke-width', 2 / currentScale);
+    mask.setAttributeNS(null, 'fill', 'black');
+    scopeGroup.appendChild(mask);
     
-    scope = document.createElementNS("http://www.w3.org/2000/svg","circle");
-	scope.setAttributeNS(null, "cx", 0);	
-	scope.setAttributeNS(null, "cy", 0);		
-	scope.setAttributeNS(null, "r", ((availablePixels - 22) / 2));
-	scope.setAttributeNS(null, "stroke", "gray");
-	scope.setAttributeNS(null, "stroke-width", "2px");
-	scope.setAttributeNS(null, "stroke-opacity", 0.5);
-	scope.setAttributeNS(null, "fill", "black");
-	scope.setAttributeNS(null, "fill-opacity", 0.0);
-	scopeGroup.appendChild(scope);
+    scopeOuterRim = document.createElementNS("http://www.w3.org/2000/svg","circle");
+	scopeOuterRim.setAttributeNS(null, "cx", 0);	
+	scopeOuterRim.setAttributeNS(null, "cy", 0);		
+	scopeOuterRim.setAttributeNS(null, "r", ((availablePixels - 22) / 2));
+	scopeOuterRim.setAttributeNS(null, "stroke", "gray");
+	scopeOuterRim.setAttributeNS(null, "stroke-width", "2px");
+	scopeOuterRim.setAttributeNS(null, "stroke-opacity", 0.5);
+	scopeOuterRim.setAttributeNS(null, "fill", "black");
+	scopeOuterRim.setAttributeNS(null, "fill-opacity", 0.0);
+	scopeGroup.appendChild(scopeOuterRim);
+	
+	scopeInnerRim = document.createElementNS("http://www.w3.org/2000/svg","circle");
+	scopeInnerRim.setAttributeNS(null, "cx", 0);	
+	scopeInnerRim.setAttributeNS(null, "cy", 0);		
+	scopeInnerRim.setAttributeNS(null, "r", ((availablePixels - 22) / 4));
+	scopeInnerRim.setAttributeNS(null, "stroke", "gray");
+	scopeInnerRim.setAttributeNS(null, "stroke-width", "2px");
+	scopeInnerRim.setAttributeNS(null, "stroke-opacity", 0.5);
+	scopeInnerRim.setAttributeNS(null, "fill", "black");
+	scopeInnerRim.setAttributeNS(null, "fill-opacity", 0.0);
+	scopeGroup.appendChild(scopeInnerRim);
+	
+	grid = document.createElementNS("http://www.w3.org/2000/svg","path");
+    grid.setAttributeNS(null, 'stroke', 'gray');  
+    grid.setAttributeNS(null, 'd', 'M '+(availablePixels - 22)/2*-1+',0 L '+(availablePixels - 22)/2+',0 M 0,'+(availablePixels - 22)/2*-1+' L 0,'+(availablePixels - 22)/2+'');
+    grid.setAttributeNS(null, 'stroke-width', '1px');
+    grid.setAttributeNS(null, 'fill', 'black');
+    scopeGroup.appendChild(grid);
 }
 
 Map.prototype.createStars = function()
