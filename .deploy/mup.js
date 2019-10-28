3522ed3f79e018cc7db54db4aa6530655eddc5a3
David Hardenbrook <davehardenbrook@yahoo.com>
2019-10-27 22:38:14 -0500

    Update mup.js
    Took all the comments out.

--- .deploy/mup.js
+++ .deploy/mup.js
@@ -1,19 +1,13 @@
 module.exports = {
   servers: {
     one: {
-      // TODO: set host address, username, and authentication method
       host: '104.248.52.121',
       username: 'root',
       pem: '~/.ssh/id_rsa'
-      // password: 'server-password'
-      // or neither for authenticate from ssh-agent
-      // I had to use this commnd to generate the ssh key:
-      // ssh-keygen -m PEM -t rsa -b 2048 -C "<your_email_address>"
     }
   },
 
   app: {
-    // TODO: change app name and path
     name: 'spacefrigates',
     path: '../',
 
@@ -26,40 +20,14 @@ module.exports = {
     },
 
     env: {
-      // TODO: Change to your app's url
-      // If you are using ssl, it needs to start with https://
       ROOT_URL: 'http://104.248.52.121',
-      // MONGO_URL: 'mongodb://mongodb/meteor',
-      // MONGO_OPLOG_URL: 'mongodb://mongodb/local',
     },
 
     docker: {
-      // change to 'abernix/meteord:base' if your app is using Meteor 1.4 - 1.5
       image: 'abernix/meteord:node-8.4.0-base',
     },
 
-    // Show progress bar while uploading bundle to server
-    // You might need to disable it on CI servers
     enableUploadProgressBar: true
   },
 
-  // mongo: {
-  //   version: '3.4.1',
-  //   servers: {
-  //    one: {}
-  //  }
-  //},
-
-  // (Optional)
-  // Use the proxy to setup ssl or to route requests to the correct
-  // app when there are several apps
-
-  // proxy: {
-  //   domains: 'mywebsite.com,www.mywebsite.com',
-
-  //   ssl: {
-  //     // Enable Let's Encrypt
-  //     letsEncryptEmail: 'email@domain.com'
-  //   }
-  // }
 };
