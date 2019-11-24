7ce976410c4d48ad129e1ca717fa0ecd9da8b7c8
David Hardenbrook <davehardenbrook@yahoo.com>
2019-11-23 23:02:36 -0600

    Added code to skip old updates

--- imports/client/client.js
+++ imports/client/client.js
@@ -24,6 +24,8 @@ Client = function Client() {
     largestUpdate = 0;
     gameMode = 'START_MODE';
     playerName = "";
+    lastTimeStamp = null;
+    lastUpdateCreatedAt = null;
 }
 
 Client.prototype.init = function() {
@@ -45,82 +47,103 @@ Client.prototype.setupStreamListeners = function() {
     // 3) What is the size of the update
     // 4) How many local commands have to be reissued
     outputStream.on('output', function(serverUpdate) {
-        // updateSize = JSON.stringify(serverUpdate).length;
-        // numberOfUpdates++;
-        // totalSizeOfUpdates = totalSizeOfUpdates + updateSize;
-        // if (updateSize < smallestUpdate || smallestUpdate == 0) {
-        //     smallestUpdate = updateSize;
-        // }
-        // if (updateSize > largestUpdate) {
-        //    largestUpdate = updateSize;
-        // }
-        // console.log("Avergae Update Size: " + Math.round(totalSizeOfUpdates / numberOfUpdates) + " Smallest Update Size: " + smallestUpdate + " Largest Update Size: " + largestUpdate);
-
         serverUpdate = unpackGameState(serverUpdate);
 
-        if (!localMode) {
-            gameObjects = engine.convertObjects(gameObjects, serverUpdate.gameState);
-        }  
+        var downloadTime = Date.now() - serverUpdate.update.createdAt;
+        
+        if (downloadTime <  50) {
 
-        ////////////////////
-        // update logging //
-        ////////////////////
+            if (!localMode) {
+                gameObjects = engine.convertObjects(gameObjects, serverUpdate.gameState);
+            }  
 
-        var updateDelta = serverUpdate.updateId - lastUpdateId;
+            ////////////////////
+            // update logging //
+            ////////////////////
 
-        if (updateDelta == 1) {
-            // This is ideal state!
-        } else if (updateDelta == 0) { 
-            // This means we got the sme update twice!
-            console.log("DUPLICATE: lastUpdateId: " + lastUpdateId + " serverUpdate.updateId: " + serverUpdate.updateId);
-        } else if (updateDelta < 0) {
-            // This means we got an old update!
-            console.log("OLD UPDATE: lastUpdateId: " + lastUpdateId + " serverUpdate.updateId: " + serverUpdate.updateId);
-        } else if (updateDelta > 1) {
-            // This means we missed an update!
-            console.log("MISSED UPDATE: lastUpdateId: " + lastUpdateId + " serverUpdate.updateId: " + serverUpdate.updateId);
-        } 
+            
+            // var processDelta = Date.now() - lastTimeStamp;
+            // var updateSize = JSON.stringify(serverUpdate).length;
+            // console.log(serverUpdate.update.id + " " + downloadTime + " " + processDelta + " " + updateSize);
 
-        /////////////////////////////////////////////////////////////////
-        // This next session is intended to reissue commands locally
-        // that have not been processed on the server yet. This has
-        // to be done because when the update is sent down the local
-        // game state in overwritten. So any commands that were issued
-        // locally are lost. I'm really not 1005 sure if this works as
-        // intended.
-        /////////////////////////////////////////////////////////////////
+            // var processDelta = Date.now() - lastTimeStamp;
+            // if (processDelta > 500) {
+            //    console.log(processDelta);
+            //}
 
-        var lastCommandServerProcessed;
+            console.log(serverUpdate.update.createdAt - lastUpdateCreatedAt + " " + downloadTime);
 
-        for (x = 0; x < serverUpdate.players.length; x++) {
-            if (serverUpdate.players[x].id == client.playerId) {
-                lastCommandServerProcessed = serverUpdate.players[x].lastSeqNum;
+            lastUpdateCreatedAt = serverUpdate.update.createdAt;
+            lastTimeStamp = Date.now();
+
+            // updateSize = JSON.stringify(serverUpdate).length;
+            // numberOfUpdates++;
+            // totalSizeOfUpdates = totalSizeOfUpdates + updateSize;
+            // if (updateSize < smallestUpdate || smallestUpdate == 0) {
+            //     smallestUpdate = updateSize;
+            // }
+            // if (updateSize > largestUpdate) {
+            //    largestUpdate = updateSize;
+            // }
+            // console.log("Avergae Update Size: " + Math.round(totalSizeOfUpdates / numberOfUpdates) + " Smallest Update Size: " + smallestUpdate + " Largest Update Size: " + largestUpdate);
+
+            // var updateDelta = serverUpdate.updateId - lastUpdateId;
+
+            //if (updateDelta == 1) {
+                // This is ideal state!
+            //} else if (updateDelta == 0) { 
+                // This means we got the sme update twice!
+            //   console.log("DUPLICATE: lastUpdateId: " + lastUpdateId + " serverUpdate.updateId: " + serverUpdate.updateId);
+            //} else if (updateDelta < 0) {
+                // This means we got an old update!
+            //    console.log("OLD UPDATE: lastUpdateId: " + lastUpdateId + " serverUpdate.updateId: " + serverUpdate.updateId);
+            //} else if (updateDelta > 1) {
+                // This means we missed an update!
+            //    console.log("MISSED UPDATE: lastUpdateId: " + lastUpdateId + " serverUpdate.updateId: " + serverUpdate.updateId);
+            //} 
+
+            /////////////////////////////////////////////////////////////////
+            // This next session is intended to reissue commands locally
+            // that have not been processed on the server yet. This has
+            // to be done because when the update is sent down the local
+            // game state in overwritten. So any commands that were issued
+            // locally are lost. I'm really not 1005 sure if this works as
+            // intended.
+            /////////////////////////////////////////////////////////////////
+
+            var lastCommandServerProcessed;
+
+            for (x = 0; x < serverUpdate.players.length; x++) {
+                if (serverUpdate.players[x].id == client.playerId) {
+                    lastCommandServerProcessed = serverUpdate.players[x].lastSeqNum;
+                }
             }
-        }
 
-        for (x = 0; x < sentCommands.length; x++) {
-            if (sentCommands[x].seqNum > lastCommandServerProcessed) {
-                commands.push(sentCommands[x]);
+            for (x = 0; x < sentCommands.length; x++) {
+                if (sentCommands[x].seqNum > lastCommandServerProcessed) {
+                    commands.push(sentCommands[x]);
+                } else {
+                    // Purge the command since the server has already run it!
+                }
+            }
+
+            // Check to see if player's ship is destroyed. If it is, switch the game to 'END_MODE'
+            var playerIsAlive = false;        
+
+            for (x = 0; x < gameObjects.length; x++) {
+                if (gameObjects[x].Id == playerShipId) {
+                    playerIsAlive = true;
+                }
+            }
+
+            if (playerIsAlive) {
+                gameMode = 'PLAY_MODE';
             } else {
-                // Purge the command since the server has already run it!
+                gameMode = 'START_MODE';
             }
-        }
+            lastUpdateId = serverUpdate.updateId;
 
-        // Check to see if player's ship is destroyed. If it is, switch the game to 'END_MODE'
-        var playerIsAlive = false;        
-
-        for (x = 0; x < gameObjects.length; x++) {
-            if (gameObjects[x].Id == playerShipId) {
-                playerIsAlive = true;
-            }
         }
-
-        if (playerIsAlive) {
-            gameMode = 'PLAY_MODE';
-        } else {
-            gameMode = 'START_MODE';
-        }
-        lastUpdateId = serverUpdate.updateId;
     });
 }
 
