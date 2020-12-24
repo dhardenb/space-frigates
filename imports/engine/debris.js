1ce05341b851ef63b499f8ba38ef1c6fcdb64bdf
David Hardenbrook <davehardenbrook@yahoo.com>
2020-12-24 00:56:05 -0500

    fixed a small bug with setting intial rotation direction

--- imports/engine/debris.js
+++ imports/engine/debris.js
@@ -27,54 +27,14 @@ export class Debris {
     }
 
     initRotation() {
-        var rotatationDirection = getRandomInt(0,1);
+        var randomNumber = getRandomInt(0,1);
 
-        if (rotatationDirection = 0) {
-            this.rotatationDirection = 'Clockwise';
+        if (randomNumber == 0) {
+            this.RotationDirection = 'Clockwise';
         } else {
-            this.rotatationDirection = 'CounterClockwise';
+            this.RotationDirection = 'CounterClockwise';
         }
 
         this.RotationVelocity = getRandomInt(1,3);
     }
 }
-
-/*
-Debris = function Debris() {
-    this.Type = "Debris";
-    this.Size = 4.0;
-}
-
-Debris.prototype.init = function(sourceObject) {
-    this.Id = engine.getNextGameObjectId();
-    this.Fuel = sourceObject.Fuel;
-    this.LocationX = sourceObject.LocationX;
-  	this.LocationY = sourceObject.LocationY;
-  	this.Facing = sourceObject.Facing;
-  	this.Heading = sourceObject.Heading;
-    this.Velocity = sourceObject.Velocity;
-    this.RotationDirection = "None";
-  	this.RotationVelocity = 0;
-
-    this.initRotation();
-}
-
-Debris.prototype.update = function() {
-    physics.findNewFacing(this);
-	  physics.moveObjectAlongVector(this);
-}
-
-Debris.prototype.initRotation = function() {
-
-    var rotatationDirection = getRandomInt(0,1);
-
-    if (rotatationDirection = 0) {
-        this.rotatationDirection = 'Clockwise';
-    } else {
-        this.rotatationDirection = 'CounterClockwise';
-    }
-
-    this.RotationVelocity = getRandomInt(1,3);
-}
-
-*/
