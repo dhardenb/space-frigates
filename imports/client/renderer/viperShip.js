17f8841a7cd028b9ab67de25ba81f73a6545762d
David Hardenbrook <davehardenbrook@yahoo.com>
2025-11-30 02:05:36 -0600

    Inline viper ship helper and remove drawHelpers

--- imports/client/renderer/viperShip.js
+++ imports/client/renderer/viperShip.js
@@ -1,6 +1,15 @@
-import {roundRectPath} from './drawHelpers.js';
-
 export const HUMAN_SHIP_BASE_LENGTH = 208;
+
+function roundRectPath(ctx, x, y, w, h, r) {
+    const radius = Math.min(r, w * 0.5, h * 0.5);
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.arcTo(x + w, y, x + w, y + h, radius);
+    ctx.arcTo(x + w, y + h, x, y + h, radius);
+    ctx.arcTo(x, y + h, x, y, radius);
+    ctx.arcTo(x, y, x + w, y, radius);
+    ctx.closePath();
+}
 
 export function drawViperShip(ctx, pixelScale) {
     const scaleFactor = pixelScale / HUMAN_SHIP_BASE_LENGTH;
