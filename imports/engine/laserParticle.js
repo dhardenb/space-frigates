df565fb3bc8da3922f58e15156b8bc962281359a
David Hardenbrook <davehardenbrook@yahoo.com>
2025-12-01 15:36:31 -0600

    Shorten laser spark travel distance

--- imports/engine/laserParticle.js
+++ imports/engine/laserParticle.js
@@ -13,7 +13,7 @@ export class LaserParticle extends Particle {
 
     init(sourceObject) {
         const directionDeg = Math.random() * 360;
-        const speed = 120 + Math.random() * 200;
+        const speed = 60 + Math.random() * 100;
         const lifetimeSeconds = 0.15 + Math.random() * 0.65;
 
         this.LocationX = sourceObject.LocationX + (Math.random() - 0.5) * 0.2;
