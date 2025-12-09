7af0dcd2c134c8791402d77bcb532e765b2beec2
David Hardenbrook <davehardenbrook@yahoo.com>
2025-12-08 21:47:24 -0600

    Update the key events to new standards

--- imports/client/keyboard.js
+++ imports/client/keyboard.js
@@ -8,111 +8,111 @@ export class Keyboard {
 
     handleKeyPressEvents(evt) {
         // BACKSPACE
-        if (evt.keyCode == 8) {
+        if (evt.key === 'Backspace') {
             if (Client.gameMode == 'START_MODE') {
                 evt.preventDefault();
                 client.playerName = client.playerName.slice(0, -1);   
             }
         }
         // ENTER - Start
-        else if(evt.keyCode==13) {
+        else if (evt.key === 'Enter') {
             evt.preventDefault();
             if (Client.gameMode == 'START_MODE') {
                 client.requestShip();
             }
         }
         // ALT key - Toggle Shields
-        else if(evt.keyCode==18) {
+        else if (evt.key === 'Alt') {
             evt.preventDefault();
             if (Client.gameMode == 'PLAY_MODE') {
                 client.commandHandler({command: 5});
             }
         }
         // M key - Toggle missiles
-        else if(evt.keyCode==77) {
+        else if (evt.key === 'm' || evt.key === 'M') {
             evt.preventDefault();
             if (Client.gameMode == 'START_MODE') {
                 if (client.playerName.length < 8) {
-                    client.playerName = client.playerName + String.fromCharCode(evt.which);
+                    client.playerName = client.playerName + evt.key.toUpperCase();
                 }
             } else if (Client.gameMode == 'PLAY_MODE') {
                 client.commandHandler({command: 'TOGGLE_MISSILES'});
             }
         }
         // SPACE_BAR - Fire
-        else if(evt.keyCode == 32) {
+        else if (evt.key === ' ') {
             evt.preventDefault();
             if (Client.gameMode == 'PLAY_MODE') {
                 client.commandHandler({command: 0});
             }
         }
         // LEFT_ARROW - Rotate CounterClockwise
-        else if(evt.keyCode == 37) {
+        else if (evt.key === 'ArrowLeft') {
             evt.preventDefault();
             if (Client.gameMode == 'PLAY_MODE') {
                 client.commandHandler({command: 1});
             }
         }
         // UP_ARROW - Forward Thruster
-        else if(evt.keyCode==38) {
+        else if (evt.key === 'ArrowUp') {
             evt.preventDefault();
             if (Client.gameMode == 'PLAY_MODE') {
                 client.commandHandler({command: 2});
             }
         }
         // RIGHT_ARROW - Rotate Clockwise
-        else if(evt.keyCode==39) {
+        else if (evt.key === 'ArrowRight') {
             evt.preventDefault();
             if (Client.gameMode == 'PLAY_MODE') {
                 client.commandHandler({command: 3});
             }
         }
         // DOWN_ARROW - Stop
-        else if(evt.keyCode==40) {
+        else if (evt.key === 'ArrowDown') {
             if (Client.gameMode == 'PLAY_MODE') {
                 evt.preventDefault();
                 client.commandHandler({command: 'RETRO_THRUST'});
             }
         }
         // A - Rotate CounterClockwise
-        else if(evt.keyCode == 65) {
+        else if (evt.key === 'a' || evt.key === 'A') {
             evt.preventDefault();
             if (Client.gameMode == 'START_MODE') {
                 if (client.playerName.length < 8) {
-                    client.playerName = client.playerName + String.fromCharCode(evt.which);
+                    client.playerName = client.playerName + evt.key.toUpperCase();
                 }
             } else if (Client.gameMode == 'PLAY_MODE') {
                 client.commandHandler({command: 1});
             }
         }
-        // D
-        else if(evt.keyCode == 68) {
+        // D - Rotate Clockwise
+        else if (evt.key === 'd' || evt.key === 'D') {
             evt.preventDefault();
             if (Client.gameMode == 'START_MODE') {
                 if (client.playerName.length < 8) {
-                    client.playerName = client.playerName + String.fromCharCode(evt.which);
+                    client.playerName = client.playerName + evt.key.toUpperCase();
                 }
             } else if (Client.gameMode == 'PLAY_MODE') {
                 client.commandHandler({command: 3});
             }
         }
-        // S
-        else if(evt.keyCode == 83) {
+        // S - Retro Thrust
+        else if (evt.key === 's' || evt.key === 'S') {
             evt.preventDefault();
             if (Client.gameMode == 'START_MODE') {
                 if (client.playerName.length < 8) {
-                    client.playerName = client.playerName + String.fromCharCode(evt.which);
+                    client.playerName = client.playerName + evt.key.toUpperCase();
                 }
             } else if (Client.gameMode == 'PLAY_MODE') {
                 client.commandHandler({command: 'RETRO_THRUST'});
             }
         }
         // Q - Lateral thrust (left-side thrusters)
-        else if(evt.keyCode == 81) {
+        else if (evt.key === 'q' || evt.key === 'Q') {
             if (Client.gameMode == 'START_MODE') {
                 evt.preventDefault();
                 if (client.playerName.length < 8) {
-                    client.playerName = client.playerName + String.fromCharCode(evt.which);
+                    client.playerName = client.playerName + evt.key.toUpperCase();
                 }
             } else if (Client.gameMode == 'PLAY_MODE') {
                 evt.preventDefault();
@@ -120,11 +120,11 @@ export class Keyboard {
             }
         }
         // E - Lateral thrust (right-side thrusters)
-        else if(evt.keyCode == 69) {
+        else if (evt.key === 'e' || evt.key === 'E') {
             if (Client.gameMode == 'START_MODE') {
                 evt.preventDefault();
                 if (client.playerName.length < 8) {
-                    client.playerName = client.playerName + String.fromCharCode(evt.which);
+                    client.playerName = client.playerName + evt.key.toUpperCase();
                 }
             } else if (Client.gameMode == 'PLAY_MODE') {
                 evt.preventDefault();
@@ -132,11 +132,11 @@ export class Keyboard {
             }
         }
         // Z - Autopilot Brake
-        else if(evt.keyCode == 90) {
+        else if (evt.key === 'z' || evt.key === 'Z') {
             evt.preventDefault();
             if (Client.gameMode == 'START_MODE') {
                 if (client.playerName.length < 8) {
-                    client.playerName = client.playerName + String.fromCharCode(evt.which);
+                    client.playerName = client.playerName + evt.key.toUpperCase();
                 }
             } else if (Client.gameMode == 'PLAY_MODE') {
                 if (evt.repeat) {
@@ -145,28 +145,28 @@ export class Keyboard {
                 client.commandHandler({command: 'BRAKE_DOWN'});
             }
         }
-        // W
-        else if(evt.keyCode == 87) {
+        // W - Forward Thruster
+        else if (evt.key === 'w' || evt.key === 'W') {
             evt.preventDefault();
             if (Client.gameMode == 'START_MODE') {
                 if (client.playerName.length < 8) {
-                    client.playerName = client.playerName + String.fromCharCode(evt.which);
+                    client.playerName = client.playerName + evt.key.toUpperCase();
                 }
             } else if (Client.gameMode == 'PLAY_MODE') {
                 client.commandHandler({command: 2});
             }
         }
-        // User typing in name
-        else if (evt.keyCode >= 65 && evt.keyCode <=90) {
+        // User typing in name (any other letter A-Z)
+        else if (evt.key.length === 1 && evt.key.match(/[a-zA-Z]/)) {
             if (Client.gameMode == 'START_MODE') {
                 evt.preventDefault();
                 if (client.playerName.length < 8) {
-                    client.playerName = client.playerName + String.fromCharCode(evt.which);
+                    client.playerName = client.playerName + evt.key.toUpperCase();
                 }  
             }
         }
-        // + Zoom In
-        else if(evt.keyCode==187) {
+        // + Zoom In (= key, with or without shift)
+        else if (evt.key === '+' || evt.key === '=') {
             evt.preventDefault();
             if (Client.gameMode == 'PLAY_MODE') {
                 if (miniMapZoomLevel < 0.0626) {
@@ -175,7 +175,7 @@ export class Keyboard {
             }
         }
         // - Zoom Out
-        else if(evt.keyCode==189) {
+        else if (evt.key === '-') {
             evt.preventDefault();
             if (Client.gameMode == 'PLAY_MODE') {
                 if (miniMapZoomLevel > 0.015625) {
